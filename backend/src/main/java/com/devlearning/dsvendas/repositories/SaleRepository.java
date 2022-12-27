d5aecad7f9618a9a109d0cbcb0edb61267fdea45
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-12-27 19:38:21 -0300

    Group by search

--- backend/src/main/java/com/devlearning/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devlearning/dsvendas/repositories/SaleRepository.java
@@ -1,11 +1,24 @@
 package com.devlearning.dsvendas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.devlearning.dsvendas.dto.SaleSuccessDTO;
+import com.devlearning.dsvendas.dto.SaleSumDTO;
 import com.devlearning.dsvendas.entities.Sale;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT new com.devlearning.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.devlearning.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
+	
 }
