f5c453afb6f301fe80e43f201ad99b32ccb2ec7f
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-12-27 12:49:24 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devlearning/dsvendas/entities/Seller.java
@@ -0,0 +1,55 @@
+package com.devlearning.dsvendas.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	public List<Sale> sales = new ArrayList<>();
+	
+	public Seller() {
+		
+	}
+
+	public Seller(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+	
+}
