914b406d73a6b262359320896b53ede7a45b0a90
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-12-27 16:52:12 -0300

    Pagination

---
+++ backend/src/main/java/com/devlearning/dsvendas/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.devlearning.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devlearning.dsvendas.dto.SaleDTO;
+import com.devlearning.dsvendas.entities.Sale;
+import com.devlearning.dsvendas.repositories.SaleRepository;
+import com.devlearning.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+}
